b8f1645c8a333f43298091534681550937b55b7d
rainyel97 <85095908+rainyel97@users.noreply.github.com>
2023-01-16 16:46:52 +0900

    목록 토글 버튼 추가

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,19 +1,33 @@
+import { useState } from "react";
 import DiaryItem from "./DiaryItem";
 
 export default function DiaryList({ diaryData, removeDiary }) {
+  const [modal, setModal] = useState(false);
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
       <h4>{diaryData.length}개의 일기가 있습니다.</h4>
-      <div>
-        {diaryData.map((data) => (
-          <DiaryItem
-            key={`diaryitem_${data.id}`}
-            {...data}
-            removeDiary={removeDiary}
-          />
-        ))}
+      <div className="modal-btn-container">
+        <button
+          className="btn btn-success"
+          onClick={() => {
+            setModal((current) => !current);
+          }}
+        >
+          {modal ? "목록 닫기" : "목록 열기"}
+        </button>
       </div>
+      {modal ? (
+        <div>
+          {diaryData.map((data) => (
+            <DiaryItem
+              key={`diaryitem_${data.id}`}
+              {...data}
+              removeDiary={removeDiary}
+            />
+          ))}
+        </div>
+      ) : null}
     </div>
   );
 }
